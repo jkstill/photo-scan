f6d11598daeb91c567736867aed7a2a44452567f
Jared Still <jkstill@gmail.com>
2026-03-01 23:36:31 -0800

    added index

--- table-photo-ai.sql
+++ table-photo-ai.sql
@@ -10,7 +10,18 @@ create table photo_ai (
   embedding    vector(1024, float32),
   created_ts   timestamp default systimestamp,
   exif_json    json,
+  exif_date_original timestamp generated always as (
+      to_timestamp(
+          nullif(
+              json_value(exif_json, '$[*]?(@.tag == "DateTimeOriginal").val' returning varchar2(20)),
+              '0000:00:00 00:00:00'
+          ), 
+          'YYYY:MM:DD HH24:MI:SS'
+      )
+  ) virtual,
   constraint uq_photo_ai unique (file_sha256)
 );
+
+create index photo_ai_exif_date_idx on photo_ai(exif_date_original);
 
 
